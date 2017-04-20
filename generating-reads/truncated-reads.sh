a98c3f1a4642fcf304454b3233551584c018c711
Kaushik Ghose <kaushik.ghose@sbgdinc.com>
2017-04-20 16:01:52 -0400

    working version of VCF filtering and generating reads demo

---
+++ generating-reads/truncated-reads.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -ex
+
+FASTA=../data/human_g1k_v37.fa.gz
+SAMPLEVCF=../data/1kg.20.22.vcf.gz
+SAMPLENAME=HG00119
+REGION_BED=region.bed
+FILTVCF=HG00119-filt.vcf.gz
+READMODEL=1kg-pcr-free.pkl
+COVERAGE=30
+READ_GEN_SEED=7
+FASTQ_PREFIX=HG00119-truncated-reads
+READ_CORRUPT_SEED=8
+
+mitty -v4 filter-variants \
+  ${SAMPLEVCF} \
+  ${SAMPLENAME} \
+  ${REGION_BED} \
+  - \
+  2> vcf-filter.log | bgzip -c > ${FILTVCF}
+
+tabix -p vcf ${FILTVCF}
+
+mitty -v4 generate-reads \
+  ${FASTA} \
+  ${FILTVCF} \
+  ${SAMPLENAME} \
+  ${REGION_BED} \
+  ${READMODEL} \
+  ${COVERAGE} \
+  ${READ_GEN_SEED} \
+  >(gzip > ${FASTQ_PREFIX}1.fq.gz) \
+   ${FASTQ_PREFIX}-lq.txt \
+   --fastq2 >(gzip > ${FASTQ_PREFIX}2.fq.gz) \
+   --truncate-to 60 \
+   --threads 2
+
+mitty -v4 corrupt-reads \
+  ${READMODEL} \
+  ${FASTQ_PREFIX}1.fq.gz >(gzip > ${FASTQ_PREFIX}-corrupt1.fq.gz) \
+  ${FASTQ_PREFIX}-lq.txt \
+  ${FASTQ_PREFIX}-corrupt-lq.txt \
+  ${READ_CORRUPT_SEED} \
+  --fastq2-in ${FASTQ_PREFIX}2.fq.gz \
+  --fastq2-out >(gzip > ${FASTQ_PREFIX}-corrupt2.fq.gz) \
+  --threads 2

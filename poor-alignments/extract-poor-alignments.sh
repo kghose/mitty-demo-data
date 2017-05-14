f43626b448eff86373d0b3095a248bf01dab970c
Kaushik Ghose <kaushik.ghose@sbgdinc.com>
2017-05-13 23:02:41 -0400

    Changed command to subset-bam

--- poor-alignments/extract-poor-alignments.sh
+++ poor-alignments/extract-poor-alignments.sh
@@ -4,11 +4,24 @@ set -ex
 FASTA=../data/human_g1k_v37.fa.gz
 FASTQ_PREFIX=../generating-reads/HG00119-reads
 BAMIN=../alignment-accuracy/HG00119-bwa.bam
-BAMOUT=HG00119-bwa-poor.bam
 
-mitty -v4 debug poor-alignments \
+BAMOUT=HG00119-bwa-snps.bam
+mitty -v4 debug subset-bam \
   ${BAMIN} \
   ${FASTQ_PREFIX}-corrupt-lq.txt \
   ${BAMOUT} \
-  5 \
+  --v-range 0 0 \
+  --reject-reference-reads \
+  --processes 2
+
+
+BAMOUT=HG00119-bwa-pool-del.bam
+mitty -v4 debug subset-bam \
+  ${BAMIN} \
+  ${FASTQ_PREFIX}-corrupt-lq.txt \
+  ${BAMOUT} \
+  --v-range -10000 -1 \
+  --d-range -5 5 \
+  --reject-d-range \
+  --reject-reference-reads \
   --processes 2

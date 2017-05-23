35fcfe74d58c079ecc8f12204294dbbe109ac233
Kaushik Ghose <kaushik.ghose@sbgdinc.com>
2017-05-23 16:35:14 -0400

    added do not index option

--- god-aligner/god-aligner.sh
+++ god-aligner/god-aligner.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+set -ex
 
 FASTA=../data/human_g1k_v37.fa.gz
 FASTQ_PREFIX=../generating-reads/HG00119-reads
 GODBAM=HG00119-god.bam
+DO_NOT_INDEX=${1}
 
 mitty -v4 god-aligner \
   ${FASTA} \
@@ -10,4 +12,4 @@ mitty -v4 god-aligner \
   ${FASTQ_PREFIX}-corrupt-lq.txt \
   ${GODBAM} \
   --fastq2 ${FASTQ_PREFIX}-corrupt2.fq.gz \
-  --threads 2
+  --threads 2 ${DO_NOT_INDEX}
